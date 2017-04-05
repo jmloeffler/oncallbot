185f27ca658feeb3af54a6aaf34dd24c21f1c1f8
Jason Loeffler <jmloeffler@gmail.com>
2017-04-05 09:06:55 -0500

    Code polish.
    Added some comments and enabled the remaining fields in SlackPost.

--- OnCallBot/Controllers/OnCallController.cs
+++ OnCallBot/Controllers/OnCallController.cs
@@ -15,17 +15,19 @@ namespace OnCallBot.Controllers
         // POST: api/OnCall
         public SlackResponse Post(FormDataCollection formData)
         {
+            //extract the url-encoded form parameters
             var token = formData["token"];
             var command = formData["command"];
             var channel_name = formData["channel_name"];
             var text = formData["text"];
             var user_name = formData["user_name"];
 
-            var request = new SlackPost() { token = token, command = command, channel_name = channel_name, text = text, user_name = user_name };
-
-            if (request.token != CloudConfigurationManager.GetSetting("SlackValidationToken"))
+            //verify that the request came from Slack and not some rando
+            if (token != CloudConfigurationManager.GetSetting("SlackValidationToken"))
                 throw new HttpResponseException(HttpStatusCode.Unauthorized);
-            
+
+            //route the request to the appropriate method
+            var request = new SlackPost() { token = token, command = command, channel_name = channel_name, text = text, user_name = user_name };
             if (request.command == "/oncall")
                 return NewOnCall(request);
             else if (request.command == "/offcall")
@@ -33,16 +35,14 @@ namespace OnCallBot.Controllers
             else if (request.command == "/whosoncall")
                 return OnCallList(request);
             else
-                return new SlackResponse { response_type = "ephemeral", text = $"I did not understand.  Try /oncall, /offcall, or /whosoncall." };
+                return new SlackResponse($"I did not understand.  Try /oncall, /offcall, or /whosoncall.");
         }
-
-        [HttpDelete]
-        public void Delete()
-        {
-            var table = GetTableReference();
-            table.DeleteIfExists();
-        }
-
+        
+        /// <summary>
+        /// Register the caller in the on-call system
+        /// </summary>
+        /// <param name="request">The request containing the payload of the Slack POST</param>
+        /// <returns>The response to be returned to Slack</returns>
         private static SlackResponse NewOnCall(SlackPost request)
         {
             var response_text = "It shall be recorded";
@@ -64,9 +64,14 @@ namespace OnCallBot.Controllers
                 response_text = "There was an error.  Use /whosoncall to verify the operation succeeded or report this to SRE.";
             }
 
-            return new SlackResponse { response_type = "ephemeral", text = response_text };
+            return new SlackResponse(response_text);
         }
 
+        /// <summary>
+        /// Remove the caller from the on-call system
+        /// </summary>
+        /// <param name="request">The request containing the payload of the Slack POST</param>
+        /// <returns>The response to be returned to Slack</returns>
         private static SlackResponse OffCall(SlackPost request)
         {
             var response_text = "You are no longer on call.";
@@ -83,19 +88,14 @@ namespace OnCallBot.Controllers
                 response_text = "There was an error removing you from the on call list.  Are you on call?  Try /whosoncall to verify or report this error to SRE.";
             }
 
-            return new SlackResponse { response_type = "ephemeral", text = response_text };
+            return new SlackResponse(response_text);
         }
 
-        private static CloudTable GetTableReference()
-        {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("oncall2_AzureStorageConnectionString"));
-            var client = storageAccount.CreateCloudTableClient();
-            var table = client.GetTableReference("OnCallEntries");
-            table.CreateIfNotExists();
-
-            return table;
-        }
-
+        /// <summary>
+        /// Retrieve the list of those who are on call currently
+        /// </summary>
+        /// <param name="request">The request containing the payload of the Slack POST</param>
+        /// <returns>The response to be returned to Slack</returns>
         private static SlackResponse OnCallList(SlackPost request)
         {
             var response_text = "";
@@ -105,6 +105,7 @@ namespace OnCallBot.Controllers
                 var query = table.CreateQuery<OnCallEntry>();
                 var list = table.ExecuteQuery(query);
 
+                //get the slack-formatted strings and join them into a single string, delimited by newline
                 response_text = string.Join("\n", list.Select(l => l.SlackString));
                 if (string.IsNullOrWhiteSpace(response_text))
                 {
@@ -115,7 +116,17 @@ namespace OnCallBot.Controllers
             {
                 response_text = "There was an error retrieving the on-call list.  Try again or report this to SRE";
             }
-            return new SlackResponse { response_type = "ephemeral", text = response_text };
+            return new SlackResponse(response_text);
+        }
+
+        private static CloudTable GetTableReference()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("oncall2_AzureStorageConnectionString"));
+            var client = storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference("OnCallEntries");
+            table.CreateIfNotExists();
+
+            return table;
         }
     }
 }
--- OnCallBot/Models/SlackPost.cs
+++ OnCallBot/Models/SlackPost.cs
@@ -3,19 +3,28 @@
     public class SlackPost
     {
         public string token;
-        //public string team_id;
-        //public string team_domain;
-        //public string channel_id;
+        public string team_id;
+        public string team_domain;
+        public string channel_id;
         public string channel_name;
-        //public string user_id;
+        public string user_id;
         public string user_name;
         public string command;
         public string text;
-        //public string response_url;
+        public string response_url;
     }
 
     public class SlackResponse
     {
+        public SlackResponse(string text) : this("empemeral", text)
+        {}
+
+        public SlackResponse(string responseType, string text)
+        {
+            this.response_type = responseType;
+            this.text = text;
+        }
+
         public string response_type { get; set; }
         public string text { get; set; }
     }
