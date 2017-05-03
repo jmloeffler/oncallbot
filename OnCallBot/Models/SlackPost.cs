7b7f259e12139b241807f562007c2296e31d04b6
Jason Loeffler <jmloeffler@gmail.com>
2017-05-03 16:41:19 -0500

    Minor structural improvements

--- OnCallBot/Models/SlackPost.cs
+++ OnCallBot/Models/SlackPost.cs
@@ -13,19 +13,4 @@
         public string text;
         public string response_url;
     }
-
-    public class SlackResponse
-    {
-        public SlackResponse(string text) : this("ephemeral", text)
-        {}
-
-        public SlackResponse(string responseType, string text)
-        {
-            this.response_type = responseType;
-            this.text = text;
-        }
-
-        public string response_type { get; set; }
-        public string text { get; set; }
-    }
 }
---
+++ OnCallBot/Models/SlackResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace OnCallBot.Models
+{
+    public class SlackResponse
+    {
+        public SlackResponse(string text) : this("ephemeral", text)
+        {}
+
+        public SlackResponse(string responseType, string text)
+        {
+            this.response_type = responseType;
+            this.text = text;
+        }
+
+        public string response_type { get; set; }
+        public string text { get; set; }
+    }
+}
